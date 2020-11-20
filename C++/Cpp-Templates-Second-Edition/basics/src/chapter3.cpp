58031ede3ba489f8c24af2280004015bbc1dcb95
danpeczek <danpeczek@gmail.com>
2020-11-20 19:00:21 +0100

    Setup for chapter3

---
+++ C++/Cpp-Templates-Second-Edition/basics/src/chapter3.cpp
@@ -0,0 +1,17 @@
+#include "chapter3.hpp"
+
+#include <iostream>
+
+#include "common_prints.hpp"
+
+namespace chapter {
+
+namespace {
+
+} // namespace
+
+void runChapter3() {
+
+}
+
+}
--- C++/Cpp-Templates-Second-Edition/main.cpp
+++ C++/Cpp-Templates-Second-Edition/main.cpp
@@ -3,9 +3,11 @@
 
 #include <chapter1.hpp>
 #include <chapter2.hpp>
+#include <chapter3.hpp>
 
 int main() {
     chapter::runChapter1();
     chapter::runChapter2();
+    chapter::runChapter3();
     return 0;
 }
