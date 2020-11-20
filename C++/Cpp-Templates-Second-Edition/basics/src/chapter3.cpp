e55d8d4adf2975a764d60c83625c209fad5e502f
danpeczek <danpeczek@gmail.com>
2020-11-20 20:41:50 +0100

    Using of template non type

--- C++/Cpp-Templates-Second-Edition/basics/src/chapter3.cpp
+++ C++/Cpp-Templates-Second-Edition/basics/src/chapter3.cpp
@@ -1,6 +1,9 @@
 #include "chapter3.hpp"
 
 #include <iostream>
+#include <string>
+
+#include "basics/stacknontype.hpp"
 
 #include "common_prints.hpp"
 
@@ -8,10 +11,28 @@ namespace chapter {
 
 namespace {
 
+void useStackNonType() {
+    printTitle("Usage of Stack none type");
+
+    basics::StackNonType<int, 20> int20Stack;
+    basics::StackNonType<int, 40> int40Stack;
+    basics::StackNonType<std::string, 40> stringStack;
+
+    int20Stack.push(7);
+    printCallFunction("int20Stack.top()", int20Stack.top());
+    int20Stack.pop();
+
+    stringStack.push("string value");
+    printCallFunction("stringStack.top()", stringStack.top());
+    stringStack.pop();
+
+    emptyLine();
+}
+
 } // namespace
 
 void runChapter3() {
-
+    useStackNonType();
 }
 
 }
