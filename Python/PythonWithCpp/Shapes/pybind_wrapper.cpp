34316f7a8591de4e7c1cf02e5b09bdbddcf360f1
danpeczek <danpeczek@gmail.com>
2020-11-27 22:25:28 +0100

    Wrapper is working

--- Python/PythonWithCpp/Shapes/pybind_wrapper.cpp
+++ Python/PythonWithCpp/Shapes/pybind_wrapper.cpp
@@ -1,10 +1,7 @@
 #include <pybind11/pybind11.h>
 #include "areas_calc.hpp"
 
-void init_shapes(pybind11::module &);
-
-PYBIND11_MODULE(pybind11_example, m) {
-    init_shapes(m);
+PYBIND11_MODULE(Shapes, m) {
     m.doc() = "pybind11 example plugin"; // Optional module docstring
     m.def("rectangle_area_py", &rectangle_area, "Calculates rectangle area");
 }
