2ca374f14911a214f73e73f3caa720a9edbf3a4d
danpeczek <danpeczek@gmail.com>
2020-12-02 18:23:12 +0100

    Setup for chapter 4. Applied Marcin CMake suggestions.

---
+++ C++/Cpp-Templates-Second-Edition/basics/include/chapter4.hpp
@@ -0,0 +1,8 @@
+#ifndef CHAPTER4_HPP
+#define CHAPTER4_HPP
+
+namespace chapter {
+    void runChapter4();   
+}
+
+#endif
--- C++/Cpp-Templates-Second-Edition/main.cpp
+++ C++/Cpp-Templates-Second-Edition/main.cpp
@@ -4,10 +4,12 @@
 #include <chapter1.hpp>
 #include <chapter2.hpp>
 #include <chapter3.hpp>
+#include <chapter4.hpp>
 
 int main() {
     chapter::runChapter1();
     chapter::runChapter2();
     chapter::runChapter3();
+    chapter::runChapter4();
     return 0;
 }
