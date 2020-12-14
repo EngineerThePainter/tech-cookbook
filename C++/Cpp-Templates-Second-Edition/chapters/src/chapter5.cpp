987c061ab95aede4e3437ea197ad3bac19adedc1
danpeczek <danpeczek@gmail.com>
2020-12-14 18:15:28 +0100

    Typename usage, initialization handling, raw array templates

--- C++/Cpp-Templates-Second-Edition/chapters/src/chapter5.cpp
+++ C++/Cpp-Templates-Second-Edition/chapters/src/chapter5.cpp
@@ -2,13 +2,59 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+
+#include <basics/arrays.hpp>
+#include <basics/printcoll.hpp>
 
 #include <common/common_prints.hpp>
 
 namespace chapters {
 
+namespace {
+
+void printingColls() {
+    common::printTitle("Print container content");
+    std::vector<std::string> container {"the good", "the bad", "the ugly"};
+    basics::printcoll<std::vector<std::string>>(container);
+    common::emptyLine();
+}
+
+template<typename T1, typename T2, typename T3>
+void arraysFoo(int a1[7], int a2[], // pointers by language rules
+               int (&a3)[42],       // reference to array of known bound
+               int (&x0)[],         // reference to array of unknown bound
+               T1 x1,               // passing by value decays
+               T2& x2, T3&& x3)     // passing by reference
+{
+    basics::MyClass<decltype(a1)>::print();
+    basics::MyClass<decltype(a2)>::print();
+    basics::MyClass<decltype(a3)>::print();
+    basics::MyClass<decltype(x0)>::print();
+    basics::MyClass<decltype(x1)>::print();
+    basics::MyClass<decltype(x2)>::print();
+    basics::MyClass<decltype(x3)>::print();
+}
+
+void arraysUsage() {
+    common::printTitle("Templates for raw arrays");
+    int a[42];
+    basics::MyClass<decltype(a)>::print();
+    extern int x[];
+    basics::MyClass<decltype(x)>::print();
+    common::printMessage("Now let's call arraysFoo()");
+    arraysFoo(a, a, a, x, x, x, x);
+    common::emptyLine();
+}
+
+int x[] = {0, 8, 15};
+
+}
+
 void runChapter5() {
     common::printTitle("C++ Templates Chapter 5");
+    printingColls();
+    arraysUsage();
     common::emptyLine();
 }
 
