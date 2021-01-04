747a6980099da45ebcabe67fecf2651bfbea8195
danpeczek <danpeczek@gmail.com>
2021-01-04 16:13:29 +0100

    Prime numbers at compile time

--- C++/Cpp-Templates-Second-Edition/chapters/src/chapter8.cpp
+++ C++/Cpp-Templates-Second-Edition/chapters/src/chapter8.cpp
@@ -3,17 +3,29 @@
 #include <iostream>
 #include <string>
 
+#include <basics/isprime.hpp>
+
 #include <common/common_prints.hpp>
 
 namespace chapters {
 
 namespace {
 
+void primeNumber() {
+    common::printTitle("Prime number at compile time");
+    std::cout<<std::boolalpha;
+    std::cout << "9 is: " << (basics::IsPrime<9>::value ? "prime" : "not prime") << std::endl;
+    std::cout << "17 is: " << (basics::IsPrime<17>::value ? "prime" : "not prime") << std::endl;
+    std::cout << "111 is: " << (basics::IsPrime<111>::value ? "prime" : "not prime") << std::endl;
+    std::cout << "53 is: " << (basics::IsPrime<53>::value ? "prime" : "not prime") << std::endl;
+    common::emptyLine();
+}
 
 } // namespace
 
 void runChapter8() {
     common::printTitle("C++ Templates Chapter 8");
+    primeNumber();
     common::emptyLine();
 }
 
