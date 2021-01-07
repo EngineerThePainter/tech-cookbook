43977907fecb8ecba178b0cd903e9294561c3715
danpeczek <danpeczek@gmail.com>
2021-01-07 17:36:35 +0100

    Inclusion model

--- C++/Cpp-Templates-Second-Edition/chapters/src/chapter9.cpp
+++ C++/Cpp-Templates-Second-Edition/chapters/src/chapter9.cpp
@@ -3,16 +3,26 @@
 #include <iostream>
 #include <string>
 
+#include <basics/inclusionModel.hpp>
+
 #include <common/common_prints.hpp>
 
 namespace chapters {
 
 namespace {
 
+void inclusionModel() {
+    common::printTitle("Inclusion model");
+    const double x = 40.4;
+    basics::printTypeof(x);
+    common::emptyLine();
+}
+
 } // namespace
 
 void runChapter9() {
     common::printTitle("C++ Templates Chapter 9");
+    inclusionModel();
     common::emptyLine();
 }
 
