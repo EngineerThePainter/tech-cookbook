013023d2d348f1b9115ce31d92a2cb57d4d97a3f
danpeczek <danpeczek@gmail.com>
2021-01-12 19:38:44 +0100

    Foreach with std::invoke, perfect invoke template for function.

--- C++/Cpp-Templates-Second-Edition/chapters/src/chapter11.cpp
+++ C++/Cpp-Templates-Second-Edition/chapters/src/chapter11.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 
 #include <basics/foreach.hpp>
+#include <basics/foreachInvoke.hpp>
+#include <basics/invoke.hpp>
 
 #include <common/common_prints.hpp>
 
@@ -16,10 +18,22 @@ void func(int i) {
     common::printCallFunction("func", i);
 }
 
+double add(const int x, const double y) {
+    std::cout << "Add " << x << " to " << y << " gives ";
+    return x+y; 
+}
+
 class FuncObj {
     public:
     void operator() (int i) const {
         common::printCallFunction("FuncObj::operator()", i);
+    }
+};
+
+class MyClass {
+    public:
+    void memfunc(int i) const {
+        common::printCallFunction("MyClass::memfunc()", i);
     }
 };
 
@@ -38,6 +52,21 @@ void supportingFunctionObjects() {
     common::emptyLine();
     common::printMessage("Lambda as callable");
     basics::foreach(primes.begin(), primes.end(), [](int i) {common::printCallFunction("lambda", i);});
+    common::printMessage("Lambda as callable and additional argument");
+    basics::foreachInvoke(primes.begin(), primes.end(), [](std::string const& prefix, int i) {
+        std::cout << prefix << i << std::endl;
+    }, "- value: ");
+    common::emptyLine();
+    common::printMessage("Class member function");
+    MyClass obj;
+    basics::foreachInvoke(primes.begin(), primes.end(), &MyClass::memfunc, obj);
+    common::emptyLine();
+}
+
+void genericCalling() {
+    common::printTitle("Generic calling");
+    basics::call(func, 42);
+    std::cout << basics::call(add, 3, 3.5) << std::endl;
     common::emptyLine();
 }
 
@@ -46,6 +75,7 @@ void supportingFunctionObjects() {
 void runChapter11() {
     common::printTitle("C++ Templates Chapter 11");
     supportingFunctionObjects();
+    genericCalling();
     common::emptyLine();
 }
 
