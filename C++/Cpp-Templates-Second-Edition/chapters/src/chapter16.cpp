9ae5a1d30fdc496429af398a1b5cffe41e650710
danpeczek <danpeczek@gmail.com>
2021-03-26 18:08:20 +0100

    Empty chapter 16 preparation

---
+++ C++/Cpp-Templates-Second-Edition/chapters/src/chapter16.cpp
@@ -0,0 +1,18 @@
+#include "chapters/chapter16.hpp"
+
+#include <iostream>
+
+#include <common/common_prints.hpp>
+
+namespace chapters {
+
+namespace {
+
+} // namespace
+
+void runChapter16() {
+    common::printTitle("C++ Templates Chapter 16");
+    common::emptyLine();
+}
+
+}
