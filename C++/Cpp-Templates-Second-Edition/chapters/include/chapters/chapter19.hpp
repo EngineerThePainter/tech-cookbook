6c2e09b853b558d43056940aa3d526575a249484
danpeczek <danpeczek@gmail.com>
2021-06-28 21:11:17 +0200

    Initial chapter commit

---
+++ C++/Cpp-Templates-Second-Edition/chapters/include/chapters/chapter19.hpp
@@ -0,0 +1,8 @@
+#ifndef CHAPTER19_HPP
+#define CHAPTER19_HPP
+
+namespace chapters {
+    void runChapter19();   
+}
+
+#endif
---
+++ C++/Cpp-Templates-Second-Edition/chapters/src/chapter19.cpp
@@ -0,0 +1,19 @@
+#include "chapters/chapter19.hpp"
+
+#include <iostream>
+
+#include <common/common_prints.hpp>
+
+namespace chapters
+{
+namespace
+{
+
+} // namespace
+
+void runChapter19()
+{
+  common::printTitle("C++ Templates Chapter 19");
+  common::emptyLine();
+}
+}
