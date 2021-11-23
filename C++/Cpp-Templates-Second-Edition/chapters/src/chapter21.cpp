1025db40e5a31d399c8dc19d387e355692d4fc2a
danpeczek <danpeczek@gmail.com>
2021-11-23 09:26:05 +0100

    base for chapter 21

---
+++ C++/Cpp-Templates-Second-Edition/chapters/src/chapter21.cpp
@@ -0,0 +1,18 @@
+#include "chapters/chapter21.hpp"
+
+#include <common/common_prints.hpp>
+
+namespace chapters
+{
+namespace
+{
+
+
+} // namespace
+
+void runChapter21()
+{
+  common::printTitle("C++ Templates Chapter 21");
+  common::emptyLine();
+}
+}
---
+++ C++/Cpp-Templates-Second-Edition/inherit/include/inherit/ebco1.hpp
@@ -0,0 +1,12 @@
+#ifndef INHERIT_EBCO1_HPP
+#define INHERIT_EBCO1_HPP
+
+#include <algorithm>
+#include <iostream>
+
+namespace inherit {
+
+
+} // namespace inherit
+
+#endif // INHERIT_EBCO1_HPP
