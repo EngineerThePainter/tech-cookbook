06e366a6113e7594ccceaacf7091f31d4c2e2b59
danpeczek <danpeczek@gmail.com>
2021-12-13 19:08:21 +0100

    bridging

--- C++/Cpp-Templates-Second-Edition/chapters/src/chapter22.cpp
+++ C++/Cpp-Templates-Second-Edition/chapters/src/chapter22.cpp
@@ -1,5 +1,9 @@
 #include "chapters/chapter22.hpp"
 
+#include <iostream>
+
+#include <bridge/forupto.hpp>
+
 #include <common/common_prints.hpp>
 
 namespace chapters
@@ -7,11 +11,24 @@ namespace chapters
 namespace
 {
 
+  void foreach1() {
+    common::printTitle("Foreach 1");
+    bridge::forUpTo(5, bridge::printInt);
+    std::cout << std::endl;
+    bridge::forUpToFunctional(5, bridge::printInt);
+    std::cout << std::endl;
+    common::printMessage("Custom function pointer implementation");
+    bridge::forUpToFunctionPointer(5, bridge::printInt);
+    std::cout << std::endl;
+    common::emptyLine();
+  }
+
 } // namespace
 
 void runChapter22()
 {
   common::printTitle("C++ Templates Chapter 22");
+  foreach1();
   common::emptyLine();
 }
 }
