9423a05b30be74dfc0d76c3603fd53b776f6dc4b
danpeczek <danpeczek@gmail.com>
2021-12-20 18:55:06 +0100

    generic push back

--- C++/Cpp-Templates-Second-Edition/typelist/include/typelist/typelist_push_back.hpp
+++ C++/Cpp-Templates-Second-Edition/typelist/include/typelist/typelist_push_back.hpp
@@ -2,17 +2,38 @@
 #define TYPELIST_TYPELISTPUSHBACK_HPP
 
 #include "typelist.hpp"
+#include "typelist_is_empty.hpp"
+#include "typelist_front.hpp"
+#include "typelist_pop_front.hpp"
+#include "typelist_push_front.hpp"
 
 namespace typelist {
 
-template<typename List, typename NewElement>
-class PushBackT;
+template<typename List, typename NewElement, bool = IsEmpty<List>::value>
+class PushBackRecT;
 
-template<typename... Elements, typename NewElement>
-class PushBackT<TypeList<Elements...>, NewElement> {
+// recursive case
+template<typename List, typename NewElement>
+class PushBackRecT<List, NewElement, false> {
+
+  using Head = Front<List>;
+  using Tail = PopFront<List>;
+  using NewTail = typename PushBackRecT<Tail, NewElement>::Type;
+
   public:
-  using Type = TypeList<Elements..., NewElement>;
+  using Type = PushFront<NewTail, Head>;
 };
+
+// basis case
+template<typename List, typename NewElement>
+class PushBackRecT<List, NewElement, true> {
+  public:
+  using Type = PushFront<List, NewElement>;
+};
+
+// generic push-back operation
+template<typename List, typename NewElement>
+class PushBackT : public PushBackRecT<List, NewElement> {};
 
 template<typename List, typename NewElement>
 using PushBack = typename PushBackT<List, NewElement>::Type;
