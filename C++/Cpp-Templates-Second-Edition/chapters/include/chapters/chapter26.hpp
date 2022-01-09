4bfd99c9a47b5b4c5f15928db2459cb05ad7bfb2
danpeczek <danpeczek@gmail.com>
2022-01-09 10:38:01 +0100

    Placeholder for chapter 26

---
+++ C++/Cpp-Templates-Second-Edition/chapters/include/chapters/chapter26.hpp
@@ -0,0 +1,8 @@
+#ifndef CHAPTER26_HPP
+#define CHAPTER26_HPP
+
+namespace chapters {
+    void runChapter26();   
+}
+
+#endif
---
+++ C++/Cpp-Templates-Second-Edition/chapters/src/chapter26.cpp
@@ -0,0 +1,20 @@
+#include "chapters/chapter26.hpp"
+
+#include <iostream>
+#include <string>
+
+#include <common/common_prints.hpp>
+
+namespace chapters
+{
+namespace
+{
+
+} // namespace
+
+void runChapter26()
+{
+  common::printTitle("C++ Templates Chapter 26 - Discriminated Unions");
+  common::emptyLine();
+}
+}
