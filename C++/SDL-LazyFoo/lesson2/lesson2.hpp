cc17fe1c2c9f364be07552c734e4de1dcb4d2bf0
danpeczek <danpeczek@gmail.com>
2022-01-15 12:31:55 +0100

    lesson 2 - loading bitmap on the screen

---
+++ C++/SDL-LazyFoo/lesson2/lesson2.hpp
@@ -0,0 +1,12 @@
+#ifndef LESSON2_HPP
+#define LESSON2_HPP
+
+namespace sdl_lazyfoo
+{
+  namespace lesson2
+  {
+    void lesson2();
+  }
+}
+
+#endif // LESSON2_HPP
--- C++/SDL-LazyFoo/main.cpp
+++ C++/SDL-LazyFoo/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 #include "lesson1/lesson1.hpp"
+#include "lesson2/lesson2.hpp"
 
-int main(int argc, char* args []) {
+int main(int argc, char *args[])
+{
+  // sdl_lazyfoo::lesson1();
+  sdl_lazyfoo::lesson2::lesson2();
 
-  sdl_lazyfoo::lesson1();
-  
   return 0;
 }
