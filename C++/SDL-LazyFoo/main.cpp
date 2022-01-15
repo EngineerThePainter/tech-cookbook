e94697ba84d49897c3f4a234a3eff22e887a2dd6
danpeczek <danpeczek@gmail.com>
2022-01-15 12:44:31 +0100

    lesson 3 - event driven programming

--- C++/SDL-LazyFoo/main.cpp
+++ C++/SDL-LazyFoo/main.cpp
@@ -2,11 +2,13 @@
 
 #include "lesson1/lesson1.hpp"
 #include "lesson2/lesson2.hpp"
+#include "lesson3/lesson3.hpp"
 
 int main(int argc, char *args[])
 {
   // sdl_lazyfoo::lesson1();
-  sdl_lazyfoo::lesson2::lesson2();
+  // sdl_lazyfoo::lesson2::lesson2();
+  sdl_lazyfoo::lesson3::lesson3();
 
   return 0;
 }
