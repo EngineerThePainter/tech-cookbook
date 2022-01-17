eafdc94704ce3de05989e9da09c04ae2960d01d7
danpeczek <danpeczek@gmail.com>
2022-01-17 18:14:27 +0100

    lesson 5

--- C++/SDL-LazyFoo/main.cpp
+++ C++/SDL-LazyFoo/main.cpp
@@ -4,12 +4,15 @@
 #include "lesson2/lesson2.hpp"
 #include "lesson3/lesson3.hpp"
 #include "lesson4/lesson4.hpp"
+#include "lesson5/lesson5.hpp"
 
-int main(int argc, char* args[]) {
+int main(int argc, char* args[])
+{
     // sdl_lazyfoo::lesson1();
     // sdl_lazyfoo::lesson2::lesson2();
     // sdl_lazyfoo::lesson3::lesson3();
-    sdl_lazyfoo::lesson4::lesson4();
+    // sdl_lazyfoo::lesson4::lesson4();
+    sdl_lazyfoo::lesson5::lesson5();
 
     return 0;
 }
