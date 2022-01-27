7e71b97e7d8221ea67fa1ab898aa6c7251894541
danpeczek <danpeczek@gmail.com>
2022-01-27 21:12:21 +0100

    solved one place

--- Algorithms/Introduction-to-Algorithms/sorting/src/insertion_sort.cpp
+++ Algorithms/Introduction-to-Algorithms/sorting/src/insertion_sort.cpp
@@ -7,8 +7,8 @@ void InsertionSort::Sort(int* array_to_sort, int number_of_elements)
   for (int j = 1; j < number_of_elements; ++j) {
     int key = array_to_sort[j];
     int i = j - 1;
-    int number_of_iterations = 0; // At least one check before
-    while (i > 0 && array_to_sort[i] > key) {
+    int number_of_iterations = 0;
+    while (i > -1 && array_to_sort[i] > key) {
       ++number_of_iterations;
       ++number_of_moves_;
       array_to_sort[i + 1] = array_to_sort[i];
