11b000bfef0c1085b98aa585f2b18b2c8e855189
danpeczek <danpeczek@gmail.com>
2022-01-29 20:55:20 +0100

    merge sort ready

--- Algorithms/Introduction-to-Algorithms/sorting/src/demo_sorting.cpp
+++ Algorithms/Introduction-to-Algorithms/sorting/src/demo_sorting.cpp
@@ -9,7 +9,7 @@ namespace sorting
 {
 void demo_sorting(SortingAlgorithm& algorithm)
 {
-  constexpr int nb_elements = 10;
+  constexpr int nb_elements = 10000;
   int* best_case = new int[nb_elements];
   int* worst_case = new int[nb_elements];
   for (int i = 0; i < nb_elements; ++i) {
@@ -22,21 +22,15 @@ void demo_sorting(SortingAlgorithm& algorithm)
   std::cout << "Best case comparisons: " << algorithm.NumberOfComparisons()
             << ", assignements: " << algorithm.NumberOfAssignements() << std::endl;
   std::cout << best_case[0] << " : " << best_case[nb_elements - 1] << std::endl;
-  for (int i = 0; i < nb_elements; ++i) {
-    std::cout << best_case[i] << ", ";
-  }
-  std::cout << std::endl;
 
   algorithm.ResetCounters();
+
   std::cout << "Worst Case: " << utils::runWithTimeMeasure(sort, worst_case, nb_elements).count() << " milliseconds"
             << std::endl;
   std::cout << "Worst case comparisons: " << algorithm.NumberOfComparisons()
             << ", assignements: " << algorithm.NumberOfAssignements() << std::endl;
   std::cout << worst_case[0] << " : " << worst_case[nb_elements - 1] << std::endl;
-  for (int i = 0; i < nb_elements; ++i) {
-    std::cout << worst_case[i] << ", ";
-  }
-  std::cout << std::endl;
+
   delete[] best_case;
   delete[] worst_case;
 }
--- Algorithms/Introduction-to-Algorithms/sorting/src/merge_sort.cpp
+++ Algorithms/Introduction-to-Algorithms/sorting/src/merge_sort.cpp
@@ -1,15 +1,16 @@
 #include "merge_sort.hpp"
 
+#include <cmath>
 #include <iostream>
 
 namespace sorting
 {
 
-void MergeSort::Sort(int* array_to_sort, int number_of_elements) { Sort(array_to_sort, 0, number_of_elements); }
+void MergeSort::Sort(int* array_to_sort, int number_of_elements) { Sort(array_to_sort, 1, number_of_elements); }
 void MergeSort::Sort(int* A, int p, int r)
 {
   if (p < r) {
-    int q = (p + r) / 2;
+    int q = std::floor((p + r) / 2);
     Sort(A, p, q);
     Sort(A, q + 1, r);
     Merge(A, p, q, r);
@@ -19,17 +20,18 @@ void MergeSort::Merge(int* A, int p, int q, int r)
 {
   int n1 = q - p + 1;
   int n2 = r - q;
+
   int* L = new int[n1];
   int* R = new int[n2];
   for (int i = 0; i < n1; ++i) {
-    L[i] = A[p + i];
+    L[i] = A[p + i - 1];
   }
 
   for (int i = 0; i < n2; ++i) {
     R[i] = A[q + i];
   }
 
-  for (int k = p, i = 0, j = 0; k < r; ++k) {
+  for (int k = p - 1, i = 0, j = 0; k < r; ++k) {
     if (i >= n1) {
       ++number_of_assignements_;
       A[k] = R[j];
@@ -55,6 +57,7 @@ void MergeSort::Merge(int* A, int p, int q, int r)
       ++j;
     }
   }
+
   delete[] L;
   delete[] R;
 }
