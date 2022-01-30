cc5a39752eb1772fadb28461cfe15ced485c73bc
danpeczek <danpeczek@gmail.com>
2022-01-30 18:22:36 +0100

    binary search preparation

---
+++ Algorithms/Introduction-to-Algorithms/searching/binary_search.hpp
@@ -0,0 +1,11 @@
+#ifndef SEARCHING_BINARY_SEARCH
+#define SEARCHING_BINARY_SEARCH
+
+namespace searching
+{
+
+void binarySearch(int* A, int number_of_elements, int searching_value);
+
+}
+
+#endif
---
+++ Algorithms/Introduction-to-Algorithms/searching/src/binary_search.cpp
@@ -0,0 +1,6 @@
+#include "binary_search.hpp"
+
+namespace searching
+{
+void binarySearch(int* A, int number_of_elements, int searching_value) {}
+} // namespace searching
