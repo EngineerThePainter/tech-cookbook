9ba27aee50b0c8c40f3404434c6c868d2cbc72a7
Daniel Pęczek <danpeczek@gmail.com>
2022-05-18 17:34:37 +0200

    simple rest service (#94)

---
+++ Java/SpringBoot/build-rest-service/src/test/java/danpe/springboot/buildrestservice/BuildRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package danpe.springboot.buildrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
