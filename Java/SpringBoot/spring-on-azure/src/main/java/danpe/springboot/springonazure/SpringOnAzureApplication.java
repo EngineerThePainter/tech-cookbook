b4fab7f6f020615df8ecd85b36c31e63b2d8ab59
danpeczek <danpeczek@gmail.com>
2022-06-16 13:19:00 +0200

    setup the project

---
+++ Java/SpringBoot/spring-on-azure/src/main/java/danpe/springboot/springonazure/SpringOnAzureApplication.java
@@ -0,0 +1,13 @@
+package danpe.springboot.springonazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOnAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOnAzureApplication.class, args);
+	}
+
+}
---
+++ Java/SpringBoot/spring-on-azure/src/test/java/danpe/springboot/springonazure/SpringOnAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package danpe.springboot.springonazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOnAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
