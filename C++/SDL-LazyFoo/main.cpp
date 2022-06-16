0a1a8eabcf717ac086ea3c058e5082381c887fb6
danpeczek <danpeczek@gmail.com>
2022-06-16 21:17:54 +0200

    removed lesson 38

--- C++/SDL-LazyFoo/main.cpp
+++ C++/SDL-LazyFoo/main.cpp
@@ -33,7 +33,6 @@
 #include <lesson31/lesson31.hpp>
 #include <lesson32/lesson32.hpp>
 #include <lesson33/lesson33.hpp>
-#include <lesson38/lesson38.hpp>
 
 int main(int argc, char* args[])
 {
@@ -133,10 +132,7 @@ int main(int argc, char* args[])
   // sdl_lazyfoo::lesson32::lesson32();
 
   // File Reading and Writing
-  // sdl_lazyfoo::lesson33::lesson33();
-
-  // Particle Engines
-  sdl_lazyfoo::lesson38::lesson38();
+  sdl_lazyfoo::lesson33::lesson33();
 
   return 0;
 }
