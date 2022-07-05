5a4810a82ee120ac57751b8b3468f596e883e703
danpeczek <danpeczek@gmail.com>
2022-07-05 22:10:05 +0200

    setup for memory leaks detection on Windows

--- C++/LearnOpenGL/main.cpp
+++ C++/LearnOpenGL/main.cpp
@@ -3,13 +3,38 @@
 #include "hello_triangle_two_triangles_different_buffers.hpp"
 #include "hello_triangle_two_triangles_different_shaders.hpp"
 
+#ifdef _DEBUG
+#ifdef _WIN32
+#define WIN_MEMORY_LEAK_CHECKER
+#define _CRTDBG_MAP_ALLOC
+#include <cstdio>
+#include <stdlib.h>
+
+#include <crtdbg.h>
+#endif // _WIN32
+#endif // _DEBUG
+
 int main(int argc, const char* argv[])
 {
+
+#ifdef WIN_MEMORY_LEAK_CHECKER
+  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+  _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+#endif
+
   int return_value = 0;
   return_value = helloTriangle();
   return_value = helloTriangleTwoTriangles();
   return_value = helloTriangleTwoTrianglesDifferentBuffers();
   return_value = helloTriangleTwoTrianglesDifferentShaders();
+
+  int* i = new int[2];
+
+#ifdef WIN_MEMORY_LEAK_CHECKER
+  _CrtDumpMemoryLeaks();
+  getchar();
+#endif // WIN_MEMORY_LEAK_CHECKER
 
   return return_value;
 }
