b4c0dd6dee16c9e2ae1eb1f042b2867fafc7251c
danpeczek <danpeczek@gmail.com>
2022-08-01 08:37:11 +0200

    saying hello

---
+++ Java/SpringBoot/spring-on-azure/src/main/java/danpe/springboot/springonazure/Greeting.java
@@ -0,0 +1,25 @@
+package danpe.springboot.springonazure;
+
+public class Greeting {
+
+    private String message;
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+            "message='" + message + '\'' +
+            '}';
+    }
+}
