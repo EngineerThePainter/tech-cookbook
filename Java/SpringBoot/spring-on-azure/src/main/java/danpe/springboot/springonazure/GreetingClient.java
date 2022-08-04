147530fd8534282aa90bc9b38b426f987e701f90
Daniel Pęczek <danpeczek@gmail.com>
2022-08-04 22:08:40 +0200

    Spring boot to azure (#104)
    * setup the project
    
    * saying hello
    
    * param handling for greetings
    
    * deployed the app

---
+++ Java/SpringBoot/spring-on-azure/src/main/java/danpe/springboot/springonazure/GreetingClient.java
@@ -0,0 +1,32 @@
+package danpe.springboot.springonazure;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GreetingClient {
+
+    private final WebClient client;
+
+    /*
+     * Spring Boot auto configure a WebClient.Builder instance
+     * */
+    public GreetingClient(WebClient.Builder builder) {
+        this.client = builder
+            .baseUrl("http://localhost:8080")
+            .build();
+    }
+
+    public Mono<String> getMessage() {
+        return this
+            .client
+            .get()
+            .uri("/hello")
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(Greeting.class)
+            .map(Greeting::getMessage);
+    }
+}
