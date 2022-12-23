64ff7b935ac6596ccae2f293f4ec2b2f32f56743
WernherVonDonut <danpeczek@gmail.com>
2022-12-23 21:14:54 +0100

    spring sleuth tutorial done

--- Java/SpringBoot/sleuth/src/main/java/wvd/springboot/sleuth/SleuthApplication.java
+++ Java/SpringBoot/sleuth/src/main/java/wvd/springboot/sleuth/SleuthApplication.java
@@ -1,16 +1,11 @@
 package wvd.springboot.sleuth;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.Tracer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("org.springframework.cloud.sleuth.Tracer")
 public class SleuthApplication {
-	@Autowired
-	private Tracer tracer;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SleuthApplication.class, args);
 	}
--- Java/SpringBoot/sleuth/src/main/java/wvd/springboot/sleuth/SleuthController.java
+++ Java/SpringBoot/sleuth/src/main/java/wvd/springboot/sleuth/SleuthController.java
@@ -1,16 +1,26 @@
 package wvd.springboot.sleuth;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Executor;
 
 @RestController
 @Slf4j
 public class SleuthController {
 
+    private final SleuthService sleuthService;
+    private final Executor executor;
+
     @Autowired
-    private SleuthService sleuthService;
+    public SleuthController(SleuthService sleuthService, Executor executor) {
+        this.sleuthService = sleuthService;
+        this.executor = executor;
+    }
 
     @RequestMapping("/")
     public String home() {
@@ -18,10 +28,43 @@ public class SleuthController {
         return "Hello World";
     }
 
+    @SneakyThrows
     @RequestMapping("/same-span")
-    public String helloSleuthSameSpan() throws InterruptedException {
+    public String helloSleuthSameSpan() {
         log.info("Same span");
         sleuthService.doSomeWorkSameSpan();
+        return "success";
+    }
+
+    @SneakyThrows
+    @RequestMapping("/new-span")
+    public String helloSleuthNewSpan() {
+        log.info("New span");
+        sleuthService.doSomeWorkNewSpan();
+        return "success";
+    }
+
+    @RequestMapping("/new-thread")
+    public String helloSleuthNewThread() {
+        log.info("New thread");
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("I'm inside the new thread - with a new span");
+        };
+        executor.execute(runnable);
+        log.info("Done with the original span again");
+        return "success";
+    }
+
+    @GetMapping("/async")
+    public String helloSleuthAsync() throws InterruptedException {
+        log.info("Before async method call");
+        sleuthService.asyncMethod();
+        log.info("After async method call");
         return "success";
     }
 }
