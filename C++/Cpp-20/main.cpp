e915340cc05261bd3386a281c3218fd6c898f9f0
WernherVonDonut <danpeczek@gmail.com>
2023-01-02 15:33:51 +0100

    likely unlikely

--- C++/Cpp-20/main.cpp
+++ C++/Cpp-20/main.cpp
@@ -1,5 +1,6 @@
 #include "designated_initializer.hpp"
 #include "init_statement_initializer_ranged_for.hpp"
+#include "likely_unlikely.hpp"
 #include "three_way_comparison.hpp"
 
 int main(int argc, const char* argv[])
@@ -7,5 +8,6 @@ int main(int argc, const char* argv[])
   showThreeWayComparison();
   showDesignatedInitializers();
   showInitStatementAndInitializerForRangedFor();
+  likely_unlikely();
   return 0;
 }
