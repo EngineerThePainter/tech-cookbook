30f296337557684f6f5c2abc17cfbdf6e28b7751
danpeczek <danpeczek@gmail.com>
2023-07-27 07:33:20 +0200

    html statements

--- Books/refactoring-improving-design-of-existing-code/chapter1/statement.py
+++ Books/refactoring-improving-design-of-existing-code/chapter1/statement.py
@@ -12,9 +12,28 @@ def render_plain_text(data):
     return result
 
 
+def statement(invoice, plays):
+    return render_plain_text(create_statement_data(invoice, plays))
+
+
+def html_statement(invoice, plays):
+    return render_html(create_statement_data(invoice, plays))
+
+
+def render_html(data):
+    result = f"<h1>Statement for {data['customer']}</h1>\n"
+    result += "<table>\n"
+    result += "<tr><th>play</th></tr><th>seats</th><th>cost</th></tr>"
+    for perf in data['performances']:
+        result += f"<tr><td>${perf['play']['name']}</td><td${perf['audience']}</td>"
+        result += f"<td>${usd(perf['amount'] / 100)}</td></tr>\n"
+    result += "</table>\n"
+    result += f"<p>Amount owned is <em>${usd(data['total_amount'] / 100)}</em></p>\n"
+    result += f"<p>You earned <em>{data['total_volume_credits']}</em> credits</p>\n"
+    return result
+
+
 def usd(number):
     return f"${number:.2f}$"
 
 
-def statement(invoice, plays):
-    return render_plain_text(create_statement_data(invoice, plays))
