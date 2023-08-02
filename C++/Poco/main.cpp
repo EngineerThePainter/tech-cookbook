4a703d9d407c5954c9cb272a0ade9553e37e06b5
wernhervondata <danpeczek@gmail.com>
2023-08-02 22:27:38 +0200

    events with poco

---
+++ C++/Poco/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include <Poco/BasicEvent.h>
+#include <Poco/Delegate.h>
+
+class Source {
+    public:
+    Poco::BasicEvent<int> event_;
+
+    void callEvent(int n) {
+        event_(this, n);
+    }
+};
+
+class Target {
+    public:
+    void onEvent(const void* sender, int& arg) {
+        std::cout << "onEvent: " << arg << std::endl;
+    }
+};
+
+int main() {
+    Source source;
+    Target target;
+
+    source.event_ += Poco::delegate(&target, &Target::onEvent);
+    source.callEvent(42);
+    source.event_ -= Poco::delegate(&target, &Target::onEvent);
+    return 0;
+}
