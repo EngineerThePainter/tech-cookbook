ee9eaf0cae3e939287286f8b3b6963bee22a0e6f
wernhervondata <danpeczek@gmail.com>
2023-08-06 14:46:35 +0200

    active object in poco

--- C++/Poco/main.cpp
+++ C++/Poco/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <utility>
 
+#include <Poco/ActiveMethod.h>
+#include <Poco/ActiveResult.h>
 #include <Poco/BasicEvent.h>
 #include <Poco/Delegate.h>
 
@@ -19,6 +22,18 @@ class Target {
     }
 };
 
+class ActiveAdder {
+    public:
+     ActiveAdder(): add(this, &ActiveAdder::addImpl) { }
+
+     Poco::ActiveMethod<int, std::pair<int, int>, ActiveAdder> add;
+
+     private:
+     int addImpl(const std::pair<int, int>& args) {
+         return args.first + args.second;
+     } 
+};
+
 int main() {
     Source source;
     Target target;
@@ -26,5 +41,10 @@ int main() {
     source.event_ += Poco::delegate(&target, &Target::onEvent);
     source.callEvent(42);
     source.event_ -= Poco::delegate(&target, &Target::onEvent);
+
+    ActiveAdder adder;
+    Poco::ActiveResult<int> result = adder.add(std::make_pair(3, 4));
+    result.wait();
+    std::cout << "Active result: " << result.data() << std::endl;
     return 0;
 }
