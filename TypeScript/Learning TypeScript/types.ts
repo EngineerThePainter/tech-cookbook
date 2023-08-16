161773eca52963c9aa62a4ed5fcb8b69ab6e4fbd
danpeczek <danpeczek@gmail.com>
2023-08-16 08:04:12 +0200

    a bit of types

---
+++ TypeScript/Learning TypeScript/types.ts
@@ -0,0 +1,19 @@
+let bestSong = Math.random() > 0.5 ? "Chain of fools" : "Respect";
+let firstName = "Whitney";
+// Understood that the firstName is string, but call to length()
+// as function is not allowed since it's just a number
+// firstName.length()
+firstName.length
+
+let lastName: string = "Coldface";
+// Not allowed - it's string already
+// lastName = 21.37; 
+
+let struct = {
+    first: "first",
+    third: "third"
+};
+
+// Not a member
+// struct.second
+
