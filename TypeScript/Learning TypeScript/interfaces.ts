6f11535cc785ac0ba83ccc7de25c543e4ae1952e
danpeczek <danpeczek@gmail.com>
2023-09-04 08:29:42 +0200

    readonly, properties and functions

---
+++ TypeScript/Learning TypeScript/interfaces.ts
@@ -0,0 +1,38 @@
+interface Poet {
+    born: number;
+    name: string;
+}
+
+let valueLater: Poet;
+
+valueLater = {
+    born: 1935,
+    name: "Sara Teasdale"
+}
+
+// interface > aliased type object
+
+interface Page {
+    readonly text: string;
+}
+
+function read(page: Page) {
+    console.log(page.text);
+    // Not allowed
+    // page.text = "text";
+}
+
+interface HasBothFunctionTypes {
+    property: () => string;
+    method(): string;
+}
+
+const hasBoth : HasBothFunctionTypes = {
+    property: () => "property",
+    method() {
+        return "method";
+    }
+}
+
+console.log(hasBoth.property());
+console.log(hasBoth.method());
