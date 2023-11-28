345f3facb15f9824ca88115167ccfd4f8e1551d0
Daniel Peczek <danpeczek@gmail.com>
2023-11-28 15:39:52 +0100

    Bare metal c book (#126)
    * initial commit for are metal c book
    
    * chapter1 done
    
    * hello world in STM32 IDE
    
    * blinking led
    
    * chapter3 done
    
    * led steeting by button program from chapter 5
    
    * excercise 5.3
    
    * morse excercise
    
    * chapter8.4
    
    * started serial output
    
    * chapter9
    
    * handling XON/XOFF signals to board
    
    * now the sending XON and XOFF works properly, but it's in blocking mode
    
    * handled overrun error pretty neat for this problem
    
    * button interrupt project
    
    * switch message upon button click
    
    * bare mental 9.4

---
+++ Embedded/bare-metal-c-book/chapter3/src/main.c
@@ -0,0 +1,36 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  WernherVonData
+  * @version V1.0
+  * @date    05-October-2023
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f0xx.h"
+#include "stm32f0xx_nucleo.h"
+
+#include <stdbool.h>
+
+int main(void)
+{
+	bool shorter_delay=true;
+	// Initializes the hardware, e.g. system clock
+	HAL_Init();
+	// Initialize the LED2 clock
+	LED2_GPIO_CLK_ENABLE();
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.Pin = LED2_PIN;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+	HAL_GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);
+	for(;;) {
+		// Toggle LED2
+		HAL_GPIO_TogglePin(LED2_GPIO_PORT, LED2_PIN);
+		HAL_Delay(shorter_delay ? 300 : 900);
+		shorter_delay = !shorter_delay;
+	}
+}
---
+++ Embedded/bare-metal-c-book/chapter8.4/src/main.c
@@ -0,0 +1,41 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+#include <stdint.h>
+
+struct Header {
+	int8_t version_ihl_;
+	int8_t dscp_ecn_;
+	int16_t total_length_;
+	int16_t identification_;
+	int16_t flags_fragment_offset_;
+	int8_t time_to_live_;
+	int8_t protocol_;
+	int16_t header_checksum_;
+	int32_t source_ip_address_;
+	int32_t destination_ip_address_;
+	int32_t options_[4];
+} __attribute__((packed));
+
+#include <assert.h>
+#include <stdio.h>
+
+#include "stm32f0xx.h"
+#include "stm32f0xx_nucleo.h"
+
+int main(void)
+{
+	struct Header header;
+	assert(288 == sizeof(header));
+	for (;;) {
+		int x;
+		x += 1;
+	}
+}
