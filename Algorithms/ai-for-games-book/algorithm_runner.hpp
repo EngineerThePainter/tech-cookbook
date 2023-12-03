814cf06e07dafd496b595d12b74742812f06302a
wernhervondata <danpeczek@gmail.com>
2023-12-03 15:14:57 +0100

    template for running the algorithms

---
+++ Algorithms/ai-for-games-book/algorithm_runner.hpp
@@ -0,0 +1,49 @@
+#include <concepts>
+
+#include "allegro.hpp"
+
+namespace aifg
+{
+template <typename T>
+concept Algorithm = requires(T t) {
+  {
+    t.Name()
+  } -> std::same_as<const std::string>;
+  {
+    t.Update(std::declval<ALLEGRO_DISPLAY*>())
+  } -> std::same_as<void>;
+};
+
+template <Algorithm T> class AlgorithmRunner
+{
+public:
+  AlgorithmRunner(const T& algorithm) : algorithm_(algorithm), finished_(false), allegro_(800, 600, algorithm_.Name())
+  {
+  }
+  AlgorithmRunner(const AlgorithmRunner&) = delete;
+  AlgorithmRunner& operator=(const AlgorithmRunner&) = delete;
+  AlgorithmRunner(AlgorithmRunner&&) = delete;
+  AlgorithmRunner& operator=(AlgorithmRunner&&) = delete;
+
+  void run()
+  {
+    ALLEGRO_EVENT event;
+    while (!finished_) {
+      al_wait_for_event(allegro_.EventQueue(), &event);
+      switch (event.type) {
+      case ALLEGRO_EVENT_TIMER:
+        algorithm_.Update(allegro_.Display());
+        break;
+      case ALLEGRO_EVENT_DISPLAY_CLOSE:
+        finished_ = true;
+        break;
+      }
+    }
+  }
+
+private:
+  T algorithm_;
+  bool finished_;
+  Allegro allegro_;
+};
+} // namespace aifg
--- Algorithms/ai-for-games-book/main.cpp
+++ Algorithms/ai-for-games-book/main.cpp
@@ -1,24 +1,10 @@
-#include "allegro.hpp"
+#include "algorithm_runner.hpp"
+#include "kinematic/seek.hpp"
 
 int main(int argc, char** argv)
 {
-  aifg::Allegro allegro(800, 600, "AI for Games");
-
-  bool finished = false;
-  while (!finished) {
-    ALLEGRO_EVENT event;
-    al_wait_for_event(allegro.EventQueue(), &event);
-
-    switch (event.type) {
-    case ALLEGRO_EVENT_TIMER:
-      al_clear_to_color(al_map_rgb(0, 0, 0));
-      al_flip_display();
-      break;
-    case ALLEGRO_EVENT_DISPLAY_CLOSE:
-      finished = true;
-      break;
-    }
-  }
-
+  aifg::Seek seek;
+  aifg::AlgorithmRunner<aifg::Seek> runner(seek);
+  runner.run();
   return 0;
 }
