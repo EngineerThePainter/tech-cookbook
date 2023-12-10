fc38c7f513ff5768dc8887460bf6617dd20029c0
wernhervondata <danpeczek@gmail.com>
2023-12-10 10:47:30 +0100

    arrive template

--- Algorithms/ai-for-games-book/main.cpp
+++ Algorithms/ai-for-games-book/main.cpp
@@ -2,12 +2,20 @@
 #include <type_traits>
 
 #include "algorithm_runner.hpp"
+#include "kinematic/arrive.hpp"
 #include "kinematic/seek.hpp"
 
 namespace
 {
-enum class Option { kExit = 0, kSeek = 1 };
+enum class Option { kExit = 0, kSeek = 1, kArrive };
+
+template <typename t> void runAlgorithm()
+{
+  t algorithm;
+  aifg::AlgorithmRunner<t> runner(algorithm);
+  runner.run();
 }
+} // namespace
 
 int main(int argc, char** argv)
 {
@@ -16,6 +24,7 @@ int main(int argc, char** argv)
   while (!end) {
     std::cout << "Select an option:" << std::endl;
     std::cout << "1. Seek" << std::endl;
+    std::cout << "2. Arrive" << std::endl;
     std::cout << "Pass 0 to exit" << std::endl;
     std::cin >> selected_option;
     Option option = static_cast<Option>(selected_option);
@@ -26,9 +35,11 @@ int main(int argc, char** argv)
       break;
     case Option::kSeek:
       std::cout << "Running seek..." << std::endl;
-      aifg::Seek seek;
-      aifg::AlgorithmRunner<aifg::Seek> runner(seek);
-      runner.run();
+      runAlgorithm<aifg::Seek>();
+      break;
+    case Option::kArrive:
+      std::cout << "Running arrive..." << std::endl;
+      runAlgorithm<aifg::Arrive>();
       break;
     }
   }
