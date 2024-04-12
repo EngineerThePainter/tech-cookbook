772d0d4ad57db0d3f38df3f9b913b417eefaf45b
wernhervondata <danpeczek@gmail.com>
2024-04-12 20:28:14 +0200

    counting in binary on expander

--- Embedded/stm32-forbot/10-port-expander/src/main.cpp
+++ Embedded/stm32-forbot/10-port-expander/src/main.cpp
@@ -34,45 +34,67 @@
 #define WRITE 0x40
 #define READ 0x41
 
-void spi_transfer(uint8_t *tx, uint16_t len)
-{
-    for (uint16_t i = 0; i < len; ++i)
-    {
-        SPI_CS_LOW;
-        while (!(SPI2_SR & SPI_SR_TXE))
-        {
-            spi_send(SPI2, tx[i]);
-        }
-        SPI_CS_HIGH;
-    }
-}
+void sendData(uint8_t *data, int n);
 
 int main(void)
 {
     rcc_periph_clock_enable(RCC_GPIOB);
     rcc_periph_clock_enable(RCC_SPI2);
 
-    gpio_mode_setup(SPI_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, SPI_SCK | SPI_SI | SPI_SO);
-    gpio_set_af(SPI_PORT, GPIO_AF0, SPI_SCK | SPI_SI | SPI_SO);
+    gpio_mode_setup(SPI_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, SPI_SCK | SPI_SO | SPI_SI);
+    gpio_set_af(SPI_PORT, GPIO_AF0, SPI_SCK | SPI_SO | SPI_SI);
 
     gpio_mode_setup(SPI_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, SPI_CS);
-    SPI_CS_HIGH;
+    gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
+
+    gpio_set(SPI_PORT, SPI_CS);
+    gpio_set(GPIOB, GPIO8);
+    spi_reset(SPI2);
 
     spi_init_master(
-        SPI_PORT,                        // SPI port
-        SPI_CR1_BAUDRATE_FPCLK_DIV_256,  // Baud rate
-        SPI_CR1_CPOL_CLK_TO_1_WHEN_IDLE, // Clock polarity
-        SPI_CR1_CPHA_CLK_TRANSITION_1,   // Clock phase
-        SPI_CR1_MSBFIRST                 // Most significant bit first
-    );
-    spi_enable(SPI2);
-    uint8_t gpio_config[3] = {WRITE, MCP_IODIR, 0xFF};
+        SPI2,
+        SPI_CR1_BAUDRATE_FPCLK_DIV_32,
+        SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
+        SPI_CR1_CPHA_CLK_TRANSITION_1,
+        SPI_CR1_MSBFIRST);
 
+    spi_enable_software_slave_management(SPI2);
+    spi_enable_ss_output(SPI2);
+    spi_set_nss_high(SPI2);
+    spi_set_unidirectional_mode(SPI2);
+    // spi_set_data_size(SPI2, SPI_CR2_DS_8BIT);
+    spi_enable(SPI2);
+
+    uint8_t init_data[3] = {WRITE, MCP_IODIR, 0xFC};
+    sendData(init_data, 3);
+    uint8_t current_value = 0;
     while (true)
     {
-        uint8_t turn_gpio0[3] = {WRITE, MCP_OLAT, 0x01};
-        spi_transfer(turn_gpio0, 3);
+        uint8_t data[3] = {WRITE, MCP_OLAT, current_value};
+        gpio_clear(GPIOB, GPIO8);
+        for (int i = 0; i < 1000000; ++i)
+        {
+            __asm("nop");
+        }
+        sendData(data, 3);
+        gpio_set(GPIOB, GPIO8);
+        for (int i = 0; i < 1000000; ++i)
+        {
+            __asm("nop");
+        }
+        current_value = current_value == 3 ? 0 : current_value + 1;
     }
-
     return 0;
+}
+
+void sendData(uint8_t *data, int n)
+{
+    gpio_clear(SPI_PORT, SPI_CS);
+    for (int i = 0; i < n; ++i)
+    {
+        spi_send8(SPI2, data[i]);
+    }
+    while (SPI_SR(SPI2) & SPI_SR_BSY)
+        ;
+    gpio_set(SPI_PORT, SPI_CS);
 }
