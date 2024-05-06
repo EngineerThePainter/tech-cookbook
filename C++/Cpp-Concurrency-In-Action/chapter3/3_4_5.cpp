04fb6dfe7cfd8e871143773f02a4f3b4641b92e1
danpeczek <danpeczek@gmail.com>
2024-05-06 11:26:29 +0200

    threadsafe stack

--- C++/Cpp-Concurrency-In-Action/chapter3/3_4_5.cpp
+++ C++/Cpp-Concurrency-In-Action/chapter3/3_4_5.cpp
@@ -1,5 +1,7 @@
 #include <exception>
 #include <memory>
+#include <mutex>
+#include <stack>
 
 /*
     Code is presenting a thread safe stack that should not allow for race condition.
@@ -14,8 +16,57 @@ template <typename T>
 class ThreadsafeStack
 {
 public:
-    ThreadsafeStack();
-    ThreadsafeStack(const ThreadsafeStack &stack);
+    ThreadsafeStack() {}
+    ThreadsafeStack(const ThreadsafeStack &stack)
+    {
+        std::lock_guard<std::mutex> lock(stack.m_);
+        data = stack.data_;
+    }
     // Assignement operator is not allowed for this stack
     ThreadsafeStack &operator=(const ThreadsafeStack &stack) = delete;
+
+    void push(T new_value)
+    {
+        std::lock_guard<std::mutex> lock(m_);
+        data.push(std::move(new_value));
+    }
+
+    std::shared_ptr<T> pop()
+    {
+        std::lock_guard<std::mutex> lock(m_);
+        if (data_.empty())
+        {
+            throw emptyStack();
+        }
+        std::shared_ptr<T> const res(std::make_shared<T>(data_.top()));
+        data_.pop();
+        return res;
+    }
+
+    void pop(T &value)
+    {
+        std::lock_guard<std::mutex> lock(m_);
+        if (data_.empty())
+        {
+            throw emptyStack();
+        }
+        value = data_.top();
+        data_.pop();
+    }
+
+    bool empty() const
+    {
+        std::lock_guard<std::mutex> lock(m_);
+        return data_.empty();
+    }
+
+private:
+    std::stack<T> data_;
+    mutable std::mutex m_;
 };
+
+int main()
+{
+
+    return 0;
+}
