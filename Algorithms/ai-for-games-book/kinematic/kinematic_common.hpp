a8edda05335dd90fffa4048663b3824a10ad166d
wernhervondata <danpeczek@gmail.com>
2025-01-07 22:50:04 +0100

    dynamic arrive was correct

--- Algorithms/ai-for-games-book/kinematic/kinematic_common.hpp
+++ Algorithms/ai-for-games-book/kinematic/kinematic_common.hpp
@@ -6,11 +6,11 @@ struct ALLEGRO_FONT;
 
 namespace aifg
 {
-const constexpr float kMaxSpeed = 350.0f;
+const constexpr float kMaxSpeed = 250.0f;
 const constexpr float kSatisfactionRadius = 50.0f;
 const constexpr float kTimeToTargetSpeed = 0.1f;
 const constexpr float kMaxRotation = 8.0f;
-const constexpr float kMaxAcceleration = 80.0f;
+const constexpr float kMaxAcceleration = 100.0f;
 const constexpr float kTime = 1.0f / 60.0f;
 const constexpr float kSlowRadius = 60.0f;
 
